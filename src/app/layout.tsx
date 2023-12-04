de64987f685c3d6722aea6d902b51fcae082e09d
may h <mehrabhoque@gmail.com>
2023-12-03 21:21:36 -0500

    Add pacifico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Pacifico } from "next/font/google";
+
+const pacificoFont = Pacifico({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-pacifico",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-gray-900 text-white">{children}</body>
+      <body className={`${pacificoFont.variable} bg-gray-900 text-white`}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,15 +1,7 @@
-import { Pacifico } from "next/font/google";
-
-const pacificoFont = Pacifico({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-pacifico",
-});
-
 export default function NavBar() {
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-800">
-      <h1 className="text-4xl font-sans">Wond</h1>
+      <h1 className="text-4xl font-pacifico">Wond</h1>
       <div>
         <a href="#" className="text-white px-4 py-2 hover:text-gray-300">
           Home
