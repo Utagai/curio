be45a25305dd7703d91244beeb73e54b84e9d24c
may h <mehrabhoque@gmail.com>
2023-12-06 00:34:02 -0500

    Add link to 'Posts' on navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-800">
       <h1 className="text-4xl font-pacifico">Wond</h1>
       <div>
-        <a href="#" className="text-white px-4 py-2 hover:text-gray-300">
-          Home
-        </a>
+        <Link
+          href="/listing"
+          className="text-white px-4 py-2 hover:text-gray-300"
+        >
+          Posts
+        </Link>
         <a href="#" className="text-white px-4 py-2 hover:text-gray-300">
           About
         </a>
