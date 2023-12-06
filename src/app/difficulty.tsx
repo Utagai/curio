1a794f292f7890aacd0798e9d3d3d6d186623733
may h <mehrabhoque@gmail.com>
2023-12-06 00:50:20 -0500

    Move the burden of margin/padding decisions to the container element of Difficulty

--- src/app/difficulty.tsx
+++ src/app/difficulty.tsx
@@ -6,9 +6,9 @@ export enum Difficulty {
 
 export default function DifficultyLabel(props: { diff: Difficulty }) {
   return (
-    <span className="m-2">
+    <span>
       <div
-        className={`${props.diff}-label inline px-2 py-1 rounded text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500`}
+        className={`${props.diff}-label inline px-2 py-1 rounded text-sm font-bold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500`}
       >
         {props.diff}
       </div>
