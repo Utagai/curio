ff4e56341821603d59253f6ec0b3129a63addc8b
may h <mehrabhoque@gmail.com>
2023-12-06 00:49:40 -0500

    Programmatically populate the posts page
    This is just from an in-memory array but it is a start.

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,21 +1,29 @@
+import { Difficulty } from "../difficulty";
+import PostCard from "./post_card";
+
 export default function Posts() {
+  const allPosts = [
+    {
+      id: 1,
+      title: "Cool Title!",
+      author: "Kim",
+      difficulty: Difficulty.EASY,
+      imageUri: "https://source.unsplash.com/random/400x200",
+    },
+  ];
   return (
     <main className="container mx-auto p-4">
       <h2 className="text-3xl font-bold text-center mb-8">Posts</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
-        <div className="bg-green rounded-lg overflow-hidden card-shadow">
-          <img
-            src="https://source.unsplash.com/random/400x200"
-            alt="preview image"
-            className="w-full"
+        {allPosts.map((post) => (
+          <PostCard
+            key={post.id}
+            title={post.title}
+            difficulty={post.difficulty}
+            imageUri={post.imageUri}
+            author={post.author}
           />
-          <div className="p-4">
-            <h3 className="font-semibold">Title by Sam</h3>
-            <span className="inline-block bg-green-200 text-green-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">
-              Easy
-            </span>
-          </div>
-        </div>
+        ))}
       </div>
 
       <div className="flex justify-center items-center space-x-1">
