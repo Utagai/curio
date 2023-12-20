603dbea74f9b17d2818aba3bf50b44fd8d7767d0
may h <mehrabhoque@gmail.com>
2023-12-20 06:21:18 -0500

    Use leaflet for the post page as well for consistency
    This was using a placeholder G-maps iframe before.

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/navigation";
 import DifficultyLabel from "../../Difficulty";
 import MemoryDB from "../../db/memory";
 import Submission from "./Submission";
+import MapContainer from "@/app/posts/new/MapContainer";
 
 export default async function Post({ params }: { params: { id: string } }) {
   const db = new MemoryDB();
@@ -30,15 +31,7 @@ export default async function Post({ params }: { params: { id: string } }) {
               className="w-full h-auto rounded-lg"
             />
           </div>
-          <div className="md:flex-1 bg-gray-700 p-2 rounded-lg shadow-drop">
-            <div className="h-full w-full" id="map">
-              <iframe
-                className="h-full w-full"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24201.453993981195!2d-73.87777722063598!3d40.691994681261235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25d0d3ab223cd%3A0x16b11fd586b90f7d!2sRoberta&#39;s!5e0!3m2!1sen!2sus!4v1701649584713!5m2!1sen!2sus"
-                loading="lazy"
-              ></iframe>
-            </div>
-          </div>
+          <MapContainer />
         </div>
 
         <div className="bg-gray-800 p-2 rounded-lg shadow-drop mb-4">
