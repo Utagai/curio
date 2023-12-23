bbf341a8f826b63961699806f4e9743a634738d3
may h <mehrabhoque@gmail.com>
2023-12-23 17:51:00 -0500

    Wrap the dynamic leaflet map with useMemo() to prevent re-rendering on every state change

--- src/app/posts/new/MapContainer.tsx
+++ src/app/posts/new/MapContainer.tsx
@@ -2,6 +2,7 @@
 
 import { Latlng } from "@/app/model/latlng";
 import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 // Do not confuse this with MapContainer from react-leaflet. This is a container
 // for the Map component in this project.
@@ -9,14 +10,18 @@ import dynamic from "next/dynamic";
 export default function MapContainer(props: {
   onMarkerChange: ((loc: Latlng) => void) | undefined;
 }) {
-  const Map = dynamic(() => import("./Map"), {
-    loading: () => (
-      <div className="flex items-center justify-center h-full">
-        <h1 className="text-3xl bg-gray-700">loading map...</h1>
-      </div>
-    ),
-    ssr: false,
-  });
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("./Map"), {
+        loading: () => (
+          <div className="flex items-center justify-center h-full">
+            <h1 className="text-3xl bg-gray-700">loading map...</h1>
+          </div>
+        ),
+        ssr: false,
+      }),
+    []
+  );
   return (
     <div className="md:flex-1 bg-gray-700 p-2 rounded-lg shadow-drop">
       <div className="w-full md:h-full aspect-square md:aspect-auto">
