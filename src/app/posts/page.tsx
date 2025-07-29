5affbeb3bf97fcda2216bde1e2282b59c165df5c
may h <mhoque95@gmail.com>
2025-07-28 20:58:19 -0400

    Sort the posts on the posts page by reverse chrono order

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -5,11 +5,12 @@ export const dynamic = "force-dynamic";
 
 export default async function Posts() {
   const posts = await getAllPosts();
+  const sortedPosts = posts.sort((a, b) => new Date(b.submittedAt).getTime() - new Date(a.submittedAt).getTime());
   return (
     <main className="container mx-auto p-4">
       <h2 className="text-3xl font-bold text-center mb-6">Posts</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
-        {posts.map((post) => (
+        {sortedPosts.map((post) => (
           <PostCard
             key={post.id}
             id={post.id}
