9771a0c92dcabfb41f8f6a4701e2a1f196bf3538
may h <mhoque95@gmail.com>
2026-03-21 18:42:43 -0400

    Add auth checks to the image route too

--- src/app/api/post/image/route.ts
+++ src/app/api/post/image/route.ts
@@ -1,9 +1,15 @@
 import { NextRequest } from "next/server";
+import { auth } from "@clerk/nextjs/server";
 import { blobStorageFactory, dbFactory } from "../../factory";
 
 const [_, blobStorage] = [dbFactory(), blobStorageFactory()];
 
 export async function GET(req: NextRequest) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
   const { searchParams } = new URL(req.url);
   const blobKey = searchParams.get("blobKey")!;
   const blob = await blobStorage.get(blobKey);
